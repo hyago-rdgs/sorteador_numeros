f357b6ebb32aac831887dac7c14fa6958ec53310
Hyago Rodrigues <hyagof16@gmail.com>
2024-09-29 14:29:43 -0300

    melhorando a inversão do status dos botões

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-let botao = document.getElementById('btn-reiniciar');
-
 function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let limite_min = parseInt(document.getElementById('de').value);
     let limite_max = parseInt(document.getElementById('ate').value);
-
     let lista_de_sorteados = [];
     
     for (let i = 0; i < quantidade; i++) {
@@ -15,12 +12,10 @@ function sortear(){
 
         lista_de_sorteados.push(numero);
     }
-
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${lista_de_sorteados}</label>`;
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: ${lista_de_sorteados}</label>`;
     
-    botao.classList.remove('container__botao-desabilitado');
-    botao.classList.add('container__botao');
+    inverter_status_botao("btn-reiniciar");
+    inverter_status_botao("btn-sortear");
 }
 function numero_aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -32,6 +27,22 @@ function reiniciar(){
     document.getElementById('ate').value = '';
     document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
 
-    botao.classList.remove('container__botao');
-    botao.classList.add('container__botao-desabilitado');
+    inverter_status_botao("btn-reiniciar");
+    inverter_status_botao("btn-sortear");
+}
+
+function inverter_status_botao(btn_class){
+    let btn = document.getElementById(btn_class);
+    
+    if (btn.classList.contains("container__botao-desabilitado")) {
+        btn.classList.remove("container__botao-desabilitado");
+        btn.classList.add("container__botao");
+
+        btn.removeAttribute("disabled");
+    } else {
+        btn.classList.remove("container__botao");
+        btn.classList.add("container__botao-desabilitado");
+
+        btn.setAttribute("disabled", true);
+    }
 }
